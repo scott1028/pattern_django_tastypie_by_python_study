1e25af55a375c742ee997724b6593c7ad822756c
scott1028 <mic1028002@gmail.com>
2014-01-09 16:37:14 +0800

    update CRUD test index.html & Comment

--- book/api/resource.py
+++ book/api/resource.py
@@ -4,13 +4,69 @@
 
 from tastypie.resources import ModelResource
 from book.models import first_book
+from tastypie.serializers import Serializer
+
+# CUD 將要求輸入使用者帳號密碼
+# from tastypie.authentication import BasicAuthentication
+# 不需驗證使用者
+from tastypie.authentication import Authentication
+
+# 根據 Django-Admin 後台所題設定的使用者權限來驗證
+# from tastypie.authorization import DjangoAuthorization
+# 不需驗證使用者權限
+from tastypie.authorization import Authorization
+
+# 似乎預設的 Ajax 就是使用 content-type: application/x-www-form-urlencoded
+import urlparse
+class urlencodeSerializer(Serializer):
+
+    formats = ['json', 'jsonp', 'xml', 'yaml', 'html', 'plist', 'urlencode']
+
+    content_types = {
+        'json': 'application/json',
+        'jsonp': 'text/javascript',
+        'xml': 'application/xml',
+        'yaml': 'text/yaml',
+        'html': 'text/html',
+        'plist': 'application/x-plist',
+        'urlencode': 'application/x-www-form-urlencoded',
+    }
+
+    # 實作轉換部分
+    def from_urlencode(self, data,options=None):
+        """ handles basic formencoded url posts """
+        qs = dict((k, v if len(v)>1 else v[0] )
+            for k, v in urlparse.parse_qs(data).iteritems())
+        return qs
+
+    def to_urlencode(self,content):
+        pass
 
 # 設定這個 API 回復的內容
 class first_book_resource(ModelResource):
     class Meta:
+    	# 搜尋資料的依據
         queryset = first_book.objects.all()
+
+        # 成為網址的 Resource
         resource_name = 'first_book'
 
-        # 定義可接受的方法
-        allowed_methods = ['get','post','put','delete']
-        pass
+        # 允許接受 Client Request 訪問的方法, 預設有 get 如果設定為 [] 將無法使用這個 Resource。
+        list_allowed_methods = ['get', 'post']
+
+        # 定義可以接受 Client Request Query String 的欄位與規則 ex: /api/first_book/?format=json&title=test
+        filtering = {
+    		'title': 'exact gt gte lt lte',
+		}
+
+		# 如果要製作 CRUD Restful API 必須在額外設定以下：資料序列化、用戶驗證、用戶權限(預設只有 ReadOnly)
+		# 參考：http://django-tastypie.readthedocs.org/en/latest/resources.html#resource-options-aka-meta
+
+        # 資料序列化, 如果要使用 Ajax Post 這邊要寫成這樣
+        serializer = urlencodeSerializer()
+
+        # 驗證用戶是誰(Authentication, BasicAuthentication, ApiKeyAuthentication, SessionAuthentication, DigestAuthentication, OAuthAuthentication, MultiAuthentication)
+        authentication = Authentication()
+
+        # 用戶的權限(Authorization, ReadOnlyAuthorization, DjangoAuthorization)
+        authorization = Authorization()
--- django_tastypie_pattern/urls.py
+++ django_tastypie_pattern/urls.py
@@ -5,14 +5,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-# tastypie
-from tastypie.api import Api
-
 # 引入剛剛定義的 tastypie resource api
 from book.api.resource import first_book_resource
-
-first_book_api=Api(api_name='first_book_api')
-first_book_api.register( first_book_resource() )
 
 urlpatterns = patterns('',
     # Examples:
@@ -21,20 +15,26 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^api/', include(first_book_api.urls)),
+    url(r'^api/', include(first_book_resource().urls)),
     # 根據 Tastypie 的 API 設計風格
     #
     # 觀看 api 有那些東西
-    # /api/{api_name}/?format=json
-    # 	/api/first_book_api/?format=json
+    # /api/{resource_name}/?format=json
+    # 	/api/first_book/?format=json
     #
     # 資料讀取
-    # /api/{api_name}/{app_name}/?format=json
-    # 	/api/first_book_api/first_book/?format=json
+    # /api/{resource_name}/?format=json
+    # 	/api/first_book/?format=json
     #
     # 資料表 schema
-    # /api/{api_name}/{app_name}/schema/?format=json
-    #	/api/first_book_api/first_book/schema/?format=json
+    # /api/{resource_name}/schema/?format=json
+    #	/api/first_book/schema/?format=json
+    #
+
+    # POST, PUT
+    # 以上都是透過 GET 方法取得資料，
+    # 當我們進行 POST / PUT 時會發生錯誤，這是因為沒有對他進行權限的設定。
+    # 參考：http://techblog.insureme.com.tw/2012/03/tastypie-django-api.html
     #
 
     # 根據上述 api 的格式，所以 api_name 要命名好！
