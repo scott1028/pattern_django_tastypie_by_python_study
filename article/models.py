44f6a4d9847a18279ebf21051adc2de2b4d753a3
scott1028 <mic1028002@gmail.com>
2014-01-14 17:57:32 +0800

    嘗試

--- article/models.py
+++ article/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 # Create your models here.
 
-# reporter belongs to article
+# reporter hasMany articles
 class Reporter(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
@@ -15,7 +15,7 @@ class Reporter(models.Model):
 
     pass
 
-# the article maye have many reporter
+# articles belongsTo reporter
 class Article(models.Model):
     headline = models.CharField(max_length=100)
     pub_date = models.DateField()
@@ -23,7 +23,6 @@ class Article(models.Model):
     # 會產生一個 reporter_id 的欄位, 如果定義名稱是 repoter 就會找 reporter_id, 如果是 reporters 就會找 reporters_id
     # oneToMany, 就是 belongs_to, Reporter 會增加一個 article_set 的屬性
     reporters = models.ForeignKey(Reporter,db_column='reporter_id')
-
 
     def __unicode__(self):
         # 會出現在後檯的 List String
