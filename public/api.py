285f47fc3416936fbfffd65c0d433124b3027132
scott.lan <scott.lan@taisys.com>
2014-05-16 01:04:06 +0800

    update code comment

--- public/api.py
+++ public/api.py
@@ -12,7 +12,14 @@ from public.models import *
 class public(ModelResource):
     # product_id = fields.CharField(attribute='product__product_id', null=True)  # 會受到 db_constraint 影響找不到物件會返還 404
     product_id = fields.CharField(attribute='product_id')  # 不會受到 db_constrains 影響
-    product_id2 = fields.CharField(attribute='product__product_id', null=True)
+
+    # MNO.objects.filter(product__product_id__contains='a9')
+
+    # 似乎無法使用真實值來做 Filter
+    # MNO.objects.filter(product_id__contains='a9')
+    # raise TypeError('Related Field got invalid lookup: %s' % lookup_type)
+
+    # product_id2 = fields.CharField(attribute='product__product_id', null=True, default='', blank=True)
     class Meta:
         queryset = MNO.objects.all()
         resource_name = 'mno'
