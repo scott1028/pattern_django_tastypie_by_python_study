395caf3e369ef8ecb5189dbc4a05fc91e41982d9
scott.lan <scott.lan@taisys.com>
2014-05-19 20:30:45 +0800

    update mode

--- public/models.py
+++ public/models.py
@@ -9,3 +9,6 @@ class Product(models.Model):
 class MNO(models.Model):
 	product = models.ForeignKey(Product, to_field='product_id', on_delete=models.SET_NULL, null=True, db_constraint=False)
 	name = models.CharField(max_length=200)
+
+class Unique(models.Model):
+	no = models.CharField(max_length=200, unique=True)
